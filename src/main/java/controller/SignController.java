93a236b73f80d267318aabc69ef7e0926d6dbb88
yikai116 <1164323901@qq.com>
2017-09-14 06:03:29 +0800

    修改登录返回信息

--- src/main/java/controller/SignController.java
+++ src/main/java/controller/SignController.java
@@ -29,170 +29,157 @@ public class SignController {
 
     /**
      * 登录
+     *
      * @param param 用户参数
      * @return 返回信息，以及用户验证信息
      */
     @RequestMapping(value = "/signIn", method = RequestMethod.POST)
-    public Response signIn(@RequestBody SignInParam param){
-        try {
-            User user = userMapper.findByPhone(param.getPhone());
-            if (user == null){
-                return Response.NO_USER;
-            }
-            if (!user.getPsw().equals(param.getPsw())){
-                return Response.PSW_ERROR;
-            }
-            User tokenUser = userMapper.findByToken(param.getToken());
-            if (tokenUser != null){
-                userMapper.updateToken(tokenUser.getPhone(),null);
-            }
-            userMapper.updateToken(param.getPhone(),param.getToken());
-            return new Response<UserInfo>().SUCCESS(user.toUserInfo());
+    public Response signIn(@RequestBody SignInParam param) {
+        User user = userMapper.findByPhone(param.getPhone());
+        if (user == null) {
+            return Response.NO_USER;
         }
-        catch (Exception e){
-            e.printStackTrace();
-            return Response.SYSTEM_ERROR;
+        if (!user.getPsw().equals(param.getPsw())) {
+            return Response.PSW_ERROR;
         }
+        User tokenUser = userMapper.findByToken(param.getToken());
+        if (tokenUser != null) {
+            userMapper.updateToken(tokenUser.getPhone(), null);
+        }
+        userMapper.updateToken(param.getPhone(), param.getToken());
+        UserInfo info = new UserInfo();
+        info.setName(user.getName());
+        info.setPhone(user.getPhone());
+        info.setAvatar(user.getAvatar());
+        return new Response<UserInfo>().SUCCESS(info);
+
     }
 
     /**
      * 注册
+     *
      * @param param 用户参数
      * @return 返回信息，以及用户验证信息
      */
     @RequestMapping(value = "/signUp", method = RequestMethod.POST)
-    public Response signUp(@RequestBody SignUpParam param){
-        try {
-            //验证验证码
-            Code code = codeMapper.findByPhone(param.getPhone());
-            if (code == null || code.getCode() == null
-                    || !code.getCode().equals(param.getVerCode())){
-                return Response.VERCODE_ERROR;
-            }
+    public Response signUp(@RequestBody SignUpParam param) {
 
-            User user = userMapper.findByPhone(param.getPhone());
-            if (user!= null){
-                return Response.USER_REGISTERED;
-            }
-            User tokenUser = userMapper.findByToken(param.getToken());
-            if (tokenUser != null){
-                userMapper.updateToken(tokenUser.getPhone(),null);
-            }
-            user = new User();
-            user.setPhone(param.getPhone());
-            user.setName("用户" + param.getPhone().substring(param.getPhone().length()-4));
-            user.setPsw(param.getPsw());
-            user.setToken(param.getToken());
-            userMapper.insert(user);
-            return new Response<UserInfo>().SUCCESS(user.toUserInfo());
+        //验证验证码
+        Code code = codeMapper.findByPhone(param.getPhone());
+        if (code == null || code.getCode() == null
+                || !code.getCode().equals(param.getVerCode())) {
+            return Response.VERCODE_ERROR;
         }
-        catch (Exception e){
-            e.printStackTrace();
-            return Response.SYSTEM_ERROR;
+
+        User user = userMapper.findByPhone(param.getPhone());
+        if (user != null) {
+            return Response.USER_REGISTERED;
         }
+        User tokenUser = userMapper.findByToken(param.getToken());
+        if (tokenUser != null) {
+            userMapper.updateToken(tokenUser.getPhone(), null);
+        }
+        user = new User();
+        user.setPhone(param.getPhone());
+        user.setName("用户" + param.getPhone().substring(param.getPhone().length() - 4));
+        user.setPsw(param.getPsw());
+        user.setToken(param.getToken());
+        String url = "http://104.236.132.15:8081/CitiCup/avatar/default.png";
+        user.setAvatar(url);
+        userMapper.insert(user);
+        UserInfo info = new UserInfo();
+        info.setName(user.getName());
+        info.setPhone(user.getPhone());
+        info.setAvatar(user.getAvatar());
+        return new Response<UserInfo>().SUCCESS(info);
+
     }
 
     /**
      * 找回密码
+     *
      * @param param 用户参数
      * @return 返回信息
      */
     @RequestMapping(value = "/findPsw", method = RequestMethod.POST)
-    public Response findPsw(@RequestBody FindPswParam param){
-        try {
-            //验证验证码
-            Code code = codeMapper.findByPhone(param.getPhone());
-            if (code == null || code.getCode() == null
-                    || !code.getCode().equals(param.getVerCode())){
-                return Response.VERCODE_ERROR;
-            }
+    public Response findPsw(@RequestBody FindPswParam param) {
+        //验证验证码
+        Code code = codeMapper.findByPhone(param.getPhone());
+        if (code == null || code.getCode() == null
+                || !code.getCode().equals(param.getVerCode())) {
+            return Response.VERCODE_ERROR;
+        }
 
-            User user = userMapper.findByPhone(param.getPhone());
-            if (user == null){
-                return Response.NO_USER;
-            }
-            user.setPsw(param.getPsw());
-            userMapper.updatePsw(param.getPhone(),param.getPsw());
-            return Response.SUCCESS;
+        User user = userMapper.findByPhone(param.getPhone());
+        if (user == null) {
+            return Response.NO_USER;
         }
-        catch (Exception e){
-            e.printStackTrace();
-            return Response.SYSTEM_ERROR;
-        }
+        user.setPsw(param.getPsw());
+        userMapper.updatePsw(param.getPhone(), param.getPsw());
+        return Response.SUCCESS;
     }
 
     /**
      * 得到注册验证码
+     *
      * @param phone 用户手机号
      * @return 返回信息
      */
     @RequestMapping(value = "getSignUpVerCode", method = RequestMethod.POST)
-    public Response getSignUpVerCode(String phone){
-        try {
-            User user = userMapper.findByPhone(phone);
-            if (user != null){
-                return Response.USER_REGISTERED;
-            }
-            return new Response<String>().SUCCESS(getCode(phone).getCode());
+    public Response getSignUpVerCode(String phone) {
+        User user = userMapper.findByPhone(phone);
+        if (user != null) {
+            return Response.USER_REGISTERED;
         }
-        catch (Exception e){
-            e.printStackTrace();
-            return Response.SYSTEM_ERROR;
-        }
+        return new Response<String>().SUCCESS(getCode(phone).getCode());
     }
 
     /**
      * 得到找回密码验证码
+     *
      * @param phone 用户手机号
      * @return 返回信息
      */
     @RequestMapping(value = "getFindPswVerCode", method = RequestMethod.POST)
-    public Response getFindPswVerCode(String phone){
-        try {
-            User user = userMapper.findByPhone(phone);
-            if (user == null){
-                return Response.NO_USER;
-            }
-            return new Response<String>().SUCCESS(getCode(phone).getCode());
+    public Response getFindPswVerCode(String phone) {
+        User user = userMapper.findByPhone(phone);
+        if (user == null) {
+            return Response.NO_USER;
         }
-        catch (Exception e){
-            e.printStackTrace();
-            return Response.SYSTEM_ERROR;
-        }
+        return new Response<String>().SUCCESS(getCode(phone).getCode());
     }
 
     /**
      * 验证token
+     *
      * @param token token
      * @return 验证结果
      */
     @RequestMapping(value = "verToken", method = RequestMethod.POST)
-    public Response verToken(String token){
-        try {
-            User user = userMapper.findByToken(token);
-            if (user == null){
-                return Response.INVALIED_TOKEN_ERROR;
-            }
-            return new Response<UserInfo>().SUCCESS(user.toUserInfo());
+    public Response verToken(String token) {
+        User user = userMapper.findByToken(token);
+        if (user == null) {
+            return Response.INVALIED_TOKEN_ERROR;
         }
-        catch (Exception e){
-            e.printStackTrace();
-            return Response.SYSTEM_ERROR;
-        }
+        UserInfo info = new UserInfo();
+        info.setName(user.getName());
+        info.setPhone(user.getPhone());
+        info.setAvatar(user.getAvatar());
+        return new Response<UserInfo>().SUCCESS(info);
     }
 
     /**
      * 辅助得到验证码
+     *
      * @param phone 手机号
      * @return 验证码
      */
-    private Code getCode(String phone){
+    private Code getCode(String phone) {
         Code oldCode = codeMapper.findByPhone(phone);
         Code newCode = new Code(phone);
-        if (oldCode != null){
+        if (oldCode != null) {
             codeMapper.update(newCode);
-        }
-        else {
+        } else {
             codeMapper.insert(newCode);
         }
         return newCode;
