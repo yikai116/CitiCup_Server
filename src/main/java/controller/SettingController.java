7b3441da57f32133392e4cb1d36e5b0dd10926a4
yikai116 <1164323901@qq.com>
2017-09-14 06:03:07 +0800

    修改头像保存信息

--- src/main/java/controller/SettingController.java
+++ src/main/java/controller/SettingController.java
@@ -23,40 +23,34 @@ public class SettingController {
 
     /**
      * 设置头像
+     *
      * @param avatar 图片信息
      * @return 返回信息，以及用户验证信息
      */
     @RequestMapping(value = "/setAvatar", method = RequestMethod.POST)
-    public Response signIn(@RequestParam MultipartFile avatar,HttpServletRequest request){
+    public Response setAvatar(@RequestParam MultipartFile avatar, HttpServletRequest request) throws Exception {
         // 保存相对路径到数据库 图片写入服务器
-        try {
-            if (avatar != null && !avatar.isEmpty()) {
-                // 获取图片的文件名
-                String fileName = avatar.getOriginalFilename();
-                // 获取图片的扩展名
-                String extensionName = fileName
-                        .substring(fileName.lastIndexOf(".") + 1);
-                // 新的图片文件名 = 获取时间戳+"."图片扩展名
-                String newFileName = String.valueOf(request.getAttribute("phone"))
-                        + "." + extensionName;
-                File file = new File(request.getSession().getServletContext().getRealPath("/avatar"),newFileName);
-                System.out.println(file.getAbsolutePath());
-                if (!file.exists()) {
-                    file.mkdirs();
-                }
-                System.out.println(file.getPath());
-                avatar.transferTo(file);
-                String url = "http://104.236.132.15:8081/CitiCup/avatar/" + newFileName;
-                userMapper.updateAvatar(String.valueOf(request.getAttribute("phone")),url);
-                return new Response<String>().SUCCESS(url);
+        if (avatar != null && !avatar.isEmpty()) {
+            // 获取图片的文件名
+            String fileName = avatar.getOriginalFilename();
+            // 获取图片的扩展名
+            String extensionName = fileName
+                    .substring(fileName.lastIndexOf(".") + 1);
+            // 新的图片文件名 = 获取时间戳+"."图片扩展名
+            String newFileName = String.valueOf(request.getAttribute("phone"))
+                    + "." + extensionName;
+            File file = new File(request.getSession().getServletContext().getRealPath("/avatar"), newFileName);
+            System.out.println(file.getAbsolutePath());
+            if (!file.exists()) {
+                file.mkdirs();
             }
-            else {
-                return Response.NO_FILE;
-            }
-        }
-        catch (IOException e){
-            e.printStackTrace();
-            return Response.UPLOAD_FAIL;
+            System.out.println(file.getPath());
+            avatar.transferTo(file);
+            String url = "http://104.236.132.15:8081/CitiCup/avatar/" + newFileName;
+            userMapper.updateAvatar(String.valueOf(request.getAttribute("phone")), url);
+            return new Response<String>().SUCCESS(url);
+        } else {
+            return Response.NO_FILE;
         }
     }
 }
